096f27d458c510cc7af54c581cbd9d93cc3e5e25
Yoav Tsook <yoavt@wix.com>
2025-09-25 14:49:52 +0300

    better server

--- trainerly-packages/trainerly-server/.build/src/handlers/coaches.d.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-export declare const checkNickname: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const createCoach: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const loginCoach: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const getCoach: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const updateCoach: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-//# sourceMappingURL=coaches.d.ts.map
--- trainerly-packages/trainerly-server/.build/src/handlers/progress.d.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-export declare const createProgress: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const getTrainerProgress: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-export declare const getCoachViewProgress: (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>;
-//# sourceMappingURL=progress.d.ts.map
